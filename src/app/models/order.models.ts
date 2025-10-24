2d7034460749a8f481a9931c3451decfa317b4f3
barakbh <barakbhamu@gmail.com>
2025-10-24 18:42:27 +0300

    chore: finalize module wiring for orders feature

--- src/app/models/order.models.ts
+++
@@ -1,5 +0,0 @@
-export interface Order {
-  id: number;
-  userId: number;
-  total: number;
-}
--- src/app/store/orders/orders.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Order } from 'src/app/models/order.models';
-
-export const loadOrders = createAction('[Orders] Load Orders');
-
-export const loadOrdersSuccess = createAction(
-    '[Orders] Load Orders Success',
-    props<{ orders: Order[] }>()
-);
