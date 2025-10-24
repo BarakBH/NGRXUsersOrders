37afa20faffb4126115402d78308f318ca9d9769
barakbh <barakbhamu@gmail.com>
2025-10-24 20:29:41 +0300

    feat(step-05): cancel in-flight details fetch on selection change using switchMap; UI shows loading and details

--- src/app/store/users/users.reducer.ts
+++ src/app/store/users/users.reducer.ts
@@ -1,10 +1,13 @@
 import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { createReducer, on } from '@ngrx/store';
-import { User } from 'src/app/models/user.models';
-import { loadUsersSuccess, setSelectedUser, upsertUser, updateUser, deleteUser } from './users.actions';
+import { User, UserDetails } from 'src/app/models/user.models';
+import { loadUsersSuccess, setSelectedUser, upsertUser, updateUser, deleteUser, loadUserDetails, loadUserDetailsSuccess, loadUserDetailsFailure } from './users.actions';
 
 export interface UsersState extends EntityState<User> {
     selectedUserId: number | null;
+    selectedUserDetails: UserDetails | null;
+    loadingUserDetails: boolean;
+    userDetailsError: string | null;
 }
 
 export const usersAdapter = createEntityAdapter<User>({ // (O(1) search)
@@ -13,6 +16,9 @@ export const usersAdapter = createEntityAdapter<User>({ // (O(1) search)
 
 export const initialState: UsersState = usersAdapter.getInitialState({
     selectedUserId: null,
+    selectedUserDetails: null,
+    loadingUserDetails: false,
+    userDetailsError: null
 });
 
 
@@ -25,11 +31,32 @@ export const usersReducer = createReducer(
     // select
     on(setSelectedUser, (state, { userId }) => ({
         ...state,
-        selectedUserId: userId
+        selectedUserId: userId,
+        // clear details when selection changes or clears
+        selectedUserDetails: null,
+        loadingUserDetails: !!userId, // show spinner only when not null
+        userDetailsError: null
     })),
 
     // CRUD
     on(upsertUser, (state, { user }) => usersAdapter.upsertOne(user, state)),
     on(updateUser, (state, { update }) => usersAdapter.updateOne(update, state)),
     on(deleteUser, (state, { id }) => usersAdapter.removeOne(id, state)),
+
+    on(loadUserDetails, (state) => ({
+        ...state,
+        loadingUserDetails: true,
+        userDetailsError: null
+    })),
+    on(loadUserDetailsSuccess, (state, { details }) => ({
+        ...state,
+        selectedUserDetails: details,
+        loadingUserDetails: false
+    })),
+    on(loadUserDetailsFailure, (state, { error }) => ({
+        ...state,
+        userDetailsError: error,
+        loadingUserDetails: false
+    }))
+
 );
